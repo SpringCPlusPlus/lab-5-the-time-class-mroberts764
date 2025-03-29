5a53d8e3c457f63b7588d2e2fd693a291bf755f6
mroberts764 <michaelr062904@gmail.com>
2025-03-29 05:16:06 -0400

    Added logic for operator functions.

--- src/Time.cpp
+++ src/Time.cpp
@@ -4,30 +4,46 @@ using namespace std;
 
 // Overload the + operator
 Time Time::operator+(const Time &other) const
-{   
+{
+    int h = hours + other.hours;
+    int m = minutes + other.minutes;
+
+    h = h + (m / 60);
+    m = m % 60;
+
+    return Time(h, m);
 }
 
 // Overload the += operator
 Time& Time::operator+=(const Time &other)
-{   
+{
+    minutes += other.minutes;
+    hours += other.hours + (minutes / 60);
+    minutes %= 60;
+    return *this; 
 }
 
 // Overload the < operator
 bool Time::operator<(const Time &other) const
 {
+    return (hours * 60 + minutes) < (other.hours * 60 + other.minutes);
 }
 
 // Overload the > operator
 bool Time::operator>(const Time &other) const
 {
+    return (hours * 60 + minutes) > (other.hours * 60 + other.minutes);
 }
 
 // Overload the == operator
 bool Time::operator==(const Time &other) const
 {
+    return (hours == other.hours) && (minutes == other.minutes);
 }
 
 // Overload the friend function operator<<
 ostream &operator<<(ostream &os, const Time &tm)
 {
+    os << tm.hours << " hours, " << tm.minutes << " minutes";
+    return os;
 }
